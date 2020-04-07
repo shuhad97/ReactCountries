ff5ac44b253c09e8175cc1519eb8ff13c02441ee
shuhad97 <30422131+shuhad97@users.noreply.github.com>
2020-04-07 21:24:03 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,123 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
+
+const App = () =>{
+
+  const [countries, setCountries] = useState('')
+  const [initData, setInitData] = useState('')
+ 
+  
+  let returnSearch 
+ 
+
+ 
+
+let returnLanguages = () => Object.values(countries[0].languages).map((language) =>{ 
+  
+    return(
+      <div>
+        <li key= {language.name}> {language.name} </li>  
+      </div> )
+
+})
+ 
+ useEffect(()=>{
+      axios.get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+          setCountries(response.data)
+          setInitData(response.data)
+               
+      })
+
+    
+
+  }, [])
+let singleCountryData = ()=>{
+ 
+  const country = countries[0]
+
+ 
+
+  return( 
+    <div>  
+                
+      <h1>{country.name}</h1>
+
+      <h2>Languages</h2>
+
+      <ul>
+      {returnLanguages()}
+      </ul>
+
+      <img src={country.flag}/>
+                      
+    </div>
+
+    )
+
+  }
+
+ let returnList = Object.values(countries).map((country, index) =>{
+    
+  
+    return(
+
+  //Create an array of the new value
+  <li key = {index}>{country.name} <button type="button" onClick= {() => {setCountries([initData[index]])}}  >Show</button></li>)
+  
+
+
+})
+
+
+
+
+  const searchFilter = (event) =>{
+  const value = event.target.value
+
+
+   
+      if(value !=='' ){
+ 
+       returnSearch = countries.filter((country)=>{
+
+
+        return country.name.includes(value)
+
+      })
+
+      if(Object.entries(returnSearch).length === 1 ){
+  
+        setCountries(returnSearch)
+
+         
+      } 
+       
+      } else {
+
+        returnSearch = initData;
+
+      }
+
+      setCountries(returnSearch)
+  }
+
+    
+ 
+
+  return(
+
+
+      <div>
+      <div><h1>{countries.length}</h1>
+         Search Country <input onChange = {searchFilter} />
+
+      </div>
+        {console.log(countries)}
+        {Object.keys(countries).length == 1  ?  singleCountryData():returnList}
+       
+  </div>
+  )
+
+}
+export default App;
